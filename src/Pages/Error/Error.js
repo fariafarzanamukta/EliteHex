74c0ee25bd21523740f92fa8a2f522a27ea20b55
fariafarzanamukta <faria15-1969@diu.edu.bd>
2022-10-27 22:57:24 +0600

    error part added

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Error = () => {
+    
     return (
         <div className='flex justify-center'>
             <img src="https://pandagila.com/wp-content/uploads/2020/08/error-404-not-found.jpg" alt="" />
